3a4bc85f4ee770fbcf04499ce0ee0ddd3f7c1bb1
federicoFrancesconi <ffedericofrancesconi@gmail.com>
2024-08-19 10:54:17 -0300

    mover /templates dentro de app /sitio

--- DescuentAr/sitio/urls.py
+++ DescuentAr/sitio/urls.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from sitio.views import *
 
-
-
 urlpatterns = [
     path('', home, name = 'home'),
-    
 ]
--- DescuentAr/sitio/views.py
+++ DescuentAr/sitio/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>PAGINA DE INICIO <h1>')
-    # return render(request, 'home.html', {})
+    #return HttpResponse('<h1>PAGINA DE INICIO <h1>')
+    return render(request, 'home.html', {})
