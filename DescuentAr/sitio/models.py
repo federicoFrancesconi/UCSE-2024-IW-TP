82190435ec40e8e8fd525c22f649a4597976b47e
federicoFrancesconi <ffedericofrancesconi@gmail.com>
2024-08-22 19:06:43 -0300

    fix de urls sitio

--- DescuentAr/sitio/models.py
+++ DescuentAr/sitio/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields import DateTimeField
+import datetime, time
 
 # Create your models here.
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=25, blank = False)
+        
+    def __str__(self):
+        return self.nombre
+
+class Descuento(models.Model):
+    nombre = models.CharField(max_length=50, blank = False)
+    descripcion = models.TextField(max_length=200, blank = False)
+    usuario_creador = models.ForeignKey(User, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, blank = True, null= True)
+    fecha_hasta = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
--- DescuentAr/sitio/views.py
+++ DescuentAr/sitio/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse    
+from django.contrib.auth.decorators import login_required
+from .forms import DescuentoForm
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+
+
+@login_required
+def crear_descuento(request):
+    if request.method == 'POST':
+        form = DescuentoForm(request.POST)
+        if form.is_valid():
+            descuento = form.save(commit=False)
+            descuento.usuario_creador = request.user
+            descuento.save()
+            return redirect('home.html')  # Redirige a una página de éxito
+    else:
+        form = DescuentoForm()
+    return render(request, 'crear_descuento.html', {'form': form})
