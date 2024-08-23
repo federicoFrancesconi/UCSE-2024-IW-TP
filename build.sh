4e79190411fbf77fd3d9f8da6e41ade4ef895a69
federicoFrancesconi <ffedericofrancesconi@gmail.com>
2024-08-22 23:43:35 -0300

    fix: borrar usuario default

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install -r ./requirements.txt
 cd $(dirname $(find . | grep manage.py$))
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --username admin --email "fedefrancesconiff@gmail.com" --noinput || true
+#python manage.py createsuperuser --username admin --email "fedefrancesconiff@gmail.com" --noinput || true
