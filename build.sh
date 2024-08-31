db386314d70eb6d5ce773b08d068e0fed56d0187
federicoFrancesconi <ffedericofrancesconi@gmail.com>
2024-08-31 11:19:15 -0300

    fix build.sh 2

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ cd $(dirname $(find . | grep manage.py$))
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-if [[ "$CREATE_SUPERUSER" = "true"]]; then
+if [ "$CREATE_SUPERUSER" == "true" ]; then
     python manage.py createsuperuser --username admin --email "fedefrancesconiff@gmail.com" --noinput || true
 fi
