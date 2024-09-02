c1d4933047ec3dedbf11c2afbc49753bd619adcf
Ulises Cravero <ulisescravero95@gmail.com>
2024-09-02 14:09:12 -0300

    add estado descuento y votos

--- DescuentAr/sitio/models.py
+++ DescuentAr/sitio/models.py
@@ -5,6 +5,12 @@ import datetime, time
 
 # Create your models here.
 
+STATES_DESCUENTO = [
+    ('publicado', 'PUBLICADO'),
+    ('revision', 'REVISION'),
+    ('eliminado', 'ELIMINADO'),
+    ('suspendido', 'SUSPENDIDO')
+]
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=25, blank = False)
@@ -18,6 +24,16 @@ class Descuento(models.Model):
     usuario_creador = models.ForeignKey(User, on_delete=models.CASCADE)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, blank = True, null= True)
     fecha_hasta = models.DateField(null=True, blank=True)
+    state = models.CharField(max_length=10, choices=STATES_DESCUENTO, verbose_name='Estado', null=True)
 
     def __str__(self):
         return self.nombre
+    
+
+class Voto(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    descuento = models.ForeignKey(Descuento, on_delete=models.CASCADE)
+    voto_positivo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.usuario} - {self.descuento} - {"Positivo" if self.voto_positivo else "Negativo"}"
