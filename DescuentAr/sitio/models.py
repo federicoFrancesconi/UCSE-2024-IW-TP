8a6b5a36b63e75165dbc282382cf9118f6a26175
federicoFrancesconi <ffedericofrancesconi@gmail.com>
2024-09-03 09:43:28 -0300

    Correccion migraciones (tirar base y volver de cero)

--- DescuentAr/sitio/models.py
+++ DescuentAr/sitio/models.py
@@ -32,7 +32,7 @@ class Descuento(models.Model):
     
 
 class Voto(models.Model):
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     descuento = models.ForeignKey(Descuento, on_delete=models.CASCADE)
     voto_positivo = models.BooleanField(default=True)
 
