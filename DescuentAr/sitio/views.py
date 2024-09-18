c75721fac888b45ec262be1be92b792a039d3a6c
federicoFrancesconi <ffedericofrancesconi@gmail.com>
2024-09-18 10:22:42 -0300

    fix: crear_descuento redirect

--- DescuentAr/sitio/views.py
+++ DescuentAr/sitio/views.py
@@ -56,7 +56,7 @@ def crear_descuento(request):
             descuento.usuario_creador = request.user
             descuento.state = 'revision'
             descuento.save()
-            return redirect('home.html')  # Redirige a una página de éxito
+            return redirect('home')
     else:
         form = DescuentoForm()
     return render(request, 'crear_descuento.html', {'form': form})
