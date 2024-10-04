160c8a0c6b161fd808103c91b6c072c79a070edd
federicoFrancesconi <ffedericofrancesconi@gmail.com>
2024-10-04 15:19:35 -0300

    categoria y datepicker en form creardescuento

--- DescuentAr/sitio/forms.py
+++ DescuentAr/sitio/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Descuento
 
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class DescuentoForm(forms.ModelForm):
     class Meta:
         model = Descuento
-        fields = ['nombre', 'fecha_hasta', 'descripcion']
+        fields = ['nombre', 'categoria', 'fecha_hasta', 'descripcion']
+        widgets = {
+            'fecha_hasta': DateInput(),
+        }
